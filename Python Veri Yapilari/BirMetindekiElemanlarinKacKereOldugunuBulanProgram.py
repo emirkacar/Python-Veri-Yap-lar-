bae9a2a9dfccb5d2af2ede5362d7c63d9ad52a74
emirkacar <emirkacar86@gmail.com>
2024-05-26 08:56:20 +0300

    Add files via upload

---
+++ Python Veri Yapilari/BirMetindekiElemanlarinKacKereOldugunuBulanProgram.py
@@ -0,0 +1,47 @@
+metin = "merhaba dunya"
+sozluk = {}
+
+for i in metin:
+    if(metin.count(i) > 1):
+        sozluk[i] = metin.count(i)
+    else:
+        sozluk[i] = metin.count(i)
+print(sozluk)
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
